6d114c444f3d04d69d7b040a217531a74b41914e
Ana Luiza Almeida <nalu.al2004@gmail.com>
2022-10-20 21:22:28 -0300

    original

---
+++ script.js
@@ -0,0 +1,14 @@
+function receberNome(){
+    let vNome = document.getElementById("nome").value
+    document.getElementById("valorNome").innerHTML = vNome
+}
+
+function receberIdade(){
+    let vIdade = document.getElementById("idade").value
+    document.getElementById("valorIdade").innerHTML = vIdade
+}
+
+function receberSalario(){
+    let vSalario = document.getElementById("salario").value
+    document.getElementById("valorSalario").innerHTML = vSalario
+}
