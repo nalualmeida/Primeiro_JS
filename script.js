a2a10ae42b1bea88a670f4128da92dececf44d8c
Ana Luiza Almeida <almeida.analuiza04@gmail.com>
2022-10-21 15:18:07 -0300

    date.getFullYear

--- script.js
+++ script.js
@@ -2,8 +2,7 @@ var txtAnoNasc = document.querySelector('#anoNasc')
 
 function calcular(){
     let anoNasc = Number(txtAnoNasc.value)
-
-    let calculo = 2022 - anoNasc
-
+    let date = new Date()
+    let calculo = date.getFullYear() - Nuber(anoNasc.value)
     idade.value = calculo
 }
